cd2b8b486aae8f41c75265c5cd38668cf26ecf3b
Frank <lzcykevin@users.noreply.github.com>
2018-02-02 16:52:53 +0800

    fix bug
    fix conf bug

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "PLEASE INPUT YOUR'S DOMAIN"
+echo "Please Input Title"
+read TitleName
+echo "Please Input Your Domain"
 read DomainUrl
 
 apt update
@@ -11,9 +13,11 @@ cd /usr/share/www/speedtest/
 wget https://github.com/lzcykevin/getspeedtest/raw/master/speed.tar.gz
 tar -xvzf speed.tar.gz
 tar -xvzf speedtest.tar.gz
+sed -i "s/HTML5 Speedtest/$TitleName/" index.html
 sed -i "s/xxxxxx/$DomainUrl/"  speedtest.conf
 mv speedtest.conf /etc/nginx/conf.d/
 rm -rf speed.tar.gz
 rm -rf speedtest.tar.gz
 chmod 777 /usr/share/www/speedtest/
 nginx -s reload 
+clear
