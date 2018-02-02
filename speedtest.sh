0645ad31f2d0cb10e0f5e4f02ae46a8e1a3a9f6d
Frank <lzcykevin@users.noreply.github.com>
2018-02-02 16:08:06 +0800

    init file
    init file

---
+++ speedtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "PLEASE INPUT YOUR'S DOMAIN"
+read DomainUrl
+
+apt update
+apt -y upgrade
+apt -y install nginx php5-common php5-fpm
+mkdir -p /usr/share/www/speedtest/
+chmod 777 /var/run/php5-fpm.sock
+cd /usr/share/www/speedtest/
+wget https://github.com/lzcykevin/getspeedtest/raw/master/speed.tar.gz
+tar -xvzf speed.tar.gz
+tar -xvzf speedtest.tar.gz
+sed -i 's/xxxxxx/$DomainUrl/'  speedtest.con
+mv speedtest.conf /etc/nginx/conf.d/
+rm -rf speed.tar.gz
+rm -rf speedtest.tar.gz
+chmod 777 /usr/share/www/speedtest/
+nginx -s reload 
